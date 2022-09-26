b5ad9b916aa340215e06e1e46e0257c08247cf2b
Tigerclaw5355 <kyleskrishaan@gmail.com>
2022-09-26 20:43:19 +0100

    Changed and made this one work but not right

--- averageofevennumbers.py
+++ averageofevennumbers.py
@@ -1,1 +1,11 @@
 #find average of first 100 even numbers
+number = 0
+avg = 0
+length = 0
+while number <= 100:
+	if number%2 == 0:
+		avg += number
+		length += 1
+	number += 1
+avg = avg / length
+print(avg)
